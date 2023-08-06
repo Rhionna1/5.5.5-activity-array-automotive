20802377a145ccbf6c2e5e4ab67290cb353f6cd0
Rhionna Landers <kasmiere@Rhionnas-Mac-mini.local>
2023-08-06 13:42:17 -0500

    added 'this' code and other code to files

--- mercurySedan.js
+++ mercurySedan.js
@@ -1,9 +1,49 @@
 //this includes the vehicle class as a module
-const VehicleModule = require("./vehicleBaseClass")
+const VehicleModule = require("./vehicleBaseClass").Vehicle
 
 //this shows how to call from this module...
-let v = new VehicleModule.Vehicle("Mercury", "Sedan", "1965", "color", "mileage");
-console.log(v.make)
+
+class Car extends VehicleModule {
+    constructor (make, model, year, color, mileage) {
+        super (make, model, yearm color, mileage);
+        this.maxPassengers = 5;
+        this.passenger =0;
+        this.numberOfWheels = 4;
+        this.maxSpeed = 160;
+        this.fuel = 10;
+        this.scheduleService = false;
+    }
+
+    checkService() {
+        if (this.mileage>30000){
+            this.scheduleService = true
+            return this.scheduleService;
+        }
+    }
+
+loadPassengers(num){
+    if (this.passenger < this.maxPassengers){
+        if ((num + this.passenger) <= this.maxPassengers) {
+            this.passenger = num;
+            return this.passenger;
+        } else {
+            console.log(this.model + " " + this.make + "not have enough space to take all passengers.");
+
+        } else {
+            console.log(this.model + " " + this.make + "is full")
+        }
+    }
+}    
+}
+
+let myCar = new Car ('mercury', 'rad_sedan', '2002', 'white', 50000)
+
+myCar.start()
+myCar.loadPassengers(5)
+myCar.stop()
+myCar.checkService()
+
+console.log(myCar)
 
 
 //After you write the derived Car class, you should test it out.
